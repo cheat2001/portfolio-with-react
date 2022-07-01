51f8c2c6116145dabdca37cf2a5a22b3f76b4fa3
cheat2001 <chansocheatsok2001@gmail.com>
2022-07-01 20:57:07 +0700

    add some styles to card make it more dope

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,11 @@ import styles from "../styles/Card.module.css";
 function Card({image,title}) {
   return (
     <div className={styles.card}>
+      <div className={styles.circleContainer}>
+      <div className={`${styles.circle1} ${styles.circle}`}></div>
+      <div className={`${styles.circle2} ${styles.circle}`}></div>
+      <div className={`${styles.circle3} ${styles.circle}`}></div>
+     </div>
      <div className={styles.imgContainer}>
         <img src={image} alt="" />
      </div>
