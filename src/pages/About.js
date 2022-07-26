df3f5b3d19272b001a68f8248fe6fb392b23f099
cheat2001 <chansocheatsok2001@gmail.com>
2022-07-26 20:48:35 +0700

    rebuild after update add title for age

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
     <Animation>
       <div className={styles.about}>
         <div className={styles.aboutMe}></div>
-        <p className={styles.content}>Hey! My name is Sok Chansocheat. I am 93*6/2+42-300 years old. I have been studying as a student at the Royal University of Phnom Penh over 3 years. I chose to major in computer science because I've loved fixing and building some technology equipment since I was a teenager, that bodes well for my interest in a job in IT. I am passionate about leaning new skills and I have completed a number of training courses. When I have some free time I like to research something on the internet, write some code, play soccer, listening to music, and sometimes I also like to play games on my phone as well.
+        <p className={styles.content}>Hey! My name is Sok Chansocheat. I am <span title='21 years old'>93*6/2+42-300</span> years old. I have been studying as a student at the Royal University of Phnom Penh over 3 years. I chose to major in computer science because I've loved fixing and building some technology equipment since I was a teenager, that bodes well for my interest in a job in IT. I am passionate about leaning new skills and I have completed a number of training courses. When I have some free time I like to research something on the internet, write some code, play soccer, listening to music, and sometimes I also like to play games on my phone as well.
         </p>
     </div>
     </Animation>
